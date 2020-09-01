d207ea781cdd866093c94a7f7e8136ebfb425bce
Pham Thuong <ptthuong@ictu.edu.vn>
2020-09-01 21:31:43 +0700

    The second commit

--- TestGit_GitHub_Demo/src/main/java/ictu/testGit_GitHub/TestGit_GitHub_Demo/App.java
+++ TestGit_GitHub_Demo/src/main/java/ictu/testGit_GitHub/TestGit_GitHub_Demo/App.java
@@ -9,5 +9,11 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int x=10;
+        int y=20;
+        System.out.println("Ket qua "+x+" + "+y + " = "+ cong(x,y));
+    }
+    public static int cong(int x, int y) {
+    	return x+y;
     }
 }
