ddb5bc1d313724aa2c7592653bea9bac2fcfe6bb
Pham Thuong <ptthuong@ictu.edu.vn>
2020-09-01 21:13:35 +0700

    The first commit

---
+++ TestGit_GitHub_Demo/src/main/java/ictu/testGit_GitHub/TestGit_GitHub_Demo/App.java
@@ -0,0 +1,13 @@
+package ictu.testGit_GitHub.TestGit_GitHub_Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
