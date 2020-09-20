3d0c2e002be5d752542acf517d372732ea69f975
Pham Thuong <ptthuong@ictu.edu.vn>
2020-09-20 16:05:01 +0700

    this is the next commit: make change in the App class

--- TestGit_GitHub_Demo/src/main/java/ictu/testGit_GitHub/TestGit_GitHub_Demo/App.java
+++ TestGit_GitHub_Demo/src/main/java/ictu/testGit_GitHub/TestGit_GitHub_Demo/App.java
@@ -7,7 +7,10 @@ public class App {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		message();
+	}
+	public static void message() {
+		System.out.println("Welcome to this class!!!");
 	}
 
 }
