3dc980cc4661186899323d7b8a007a8ede65c9eb
Pham Thuong <ptthuong@ictu.edu.vn>
2020-09-20 15:57:10 +0700

    This is the next commit: edit the Hello class

--- TestGit_GitHub_Demo/src/main/java/ictu/testGit_GitHub/TestGit_GitHub_Demo/App.java
+++ TestGit_GitHub_Demo/src/main/java/ictu/testGit_GitHub/TestGit_GitHub_Demo/App.java
@@ -5,55 +5,9 @@ public class App {
 	private int y;
 	public int z;
 
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		int x = 10;
-		int y = 20;
-		int z = 90;
-		System.out.println("Ket qua " + x + " + " + y + " = " + cong(x, y, z));
 	}
 
-	public static int cong(int x, int y, int z) {
-		return x + y + z;
-	}
-
-	public static int cong(int x, int y) {
-		return x + y;
-	}
-
-	public static int tru(int x, int y) {
-		return x - y;
-	}
-
-	public static int nhan(int x, int y) {
-		return x * y;
-	}
-
-	public static int chia(int x, int y) {
-		return x / y;
-	}
-	
-	public static int tru(int x, int y, int z) {
-		return x - y-z;
-	}
-	
-	public static int nhan(int x, int y, int z) {
-		return x*y*z;
-	}
 }
